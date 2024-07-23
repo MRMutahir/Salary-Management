e7091c62d23be2fb7bb23c32dc27d13dfdea483d
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-07-23 19:12:35 +0500

    m-01-addUserSchema

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UsersSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [15, "Password must be at least 15 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Users = mongoose.model("Users", UsersSchema);
+
+export { Users };
