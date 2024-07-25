af52dbcbb5eee08871b390e2a7ccb37e24bf152d
MRMutahir <mutahirkareem820@gmail.com>
2024-07-25 13:07:25 +0500

    Login or sign up has been completed with testing.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,61 @@
+import {
+  comparePassword,
+  hashPassword,
+  sendResponse,
+} from "../helpers/common.js";
 import { registerUser, findUser } from "../services/auth.js";
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
+  
   if (!email) {
-    res.send("email is required");
-  }
-  if (!password) {
-    res.send("password is required");
+      return sendResponse(res, "Email is required", false, 400);
   }
 
+  if (!password) {
+      return sendResponse(res, "Password is required", false, 400);
+  }
+  console.log('password', password)
+
   try {
-    const existingUser = await findUser({ email });
-    if (existingUser) {
-       // 409 Conflict is more appropriate for this scenario
-      return res.status(409).send("Please choose another email address because this email is already in use.");}
-    const user = await registerUser({ email, password });
-    if (user) {
-      return res.status(201).send("User created successfully");
+      const existingUser = await findUser({ email });
+      if (existingUser) {
+          return sendResponse(res, "Please choose another email address because this email is already in use.", false, 409);
+      }
+      const secretPassword = await hashPassword(password);
+      const user = await registerUser({ email, password: secretPassword });
+
+      if (user) {
+          return sendResponse(res, "User registered successfully", true, 201);
+      } else {
+          return sendResponse(res, "Registration failed", false, 500);
+      }
+  } catch (error) {
+      next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await findUser({ email });
+
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      sendResponse(res, "Invalid Password", true, 200);
     }
+
+    sendResponse(
+      res,
+      "Congratulations, you have successfully logged in",
+      true,
+      200
+    );
   } catch (error) {
     next(error);
   }
 };
 
-export { register };
+export { register, login };
