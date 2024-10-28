62947a394d16eb2f7710deb94f10bab4b522f1a3
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-10-28 15:27:36 +0500

    test

--- routes/routes.js
+++ routes/routes.js
@@ -17,12 +17,9 @@ const routes = async (app) => {
   app.use("/api/v1/auth/verifications", verificationRoutes);
   app.use("/api/v1/users", authenticate, userRoutes);
   app.use("/api/v1/users/work", authenticate, userWorkRoutes);
-<<<<<<< HEAD
   app.use("/api/v1/users/year-manage", authenticate, userYearRoute);
-=======
   app.use("/api/v1/users/education", authenticate, educationRoutes);
   app.use("/api/v1/users/aggregation", authenticate, userAggregationRoutes);
->>>>>>> ed25b75aa1374923f23cb8a3365a743b187f3fba
   app.use((req, res, next) => {
     res.send("Route does not exist");
   });
